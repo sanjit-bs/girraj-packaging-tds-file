54b84417619e327674e5c38cf6577081687af2b1
sanjit-bs <sanjitava.22260402016@cuj.ac.in>
2026-08-31 17:19:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -427,7 +427,7 @@ COLUMNS3 = [
 COMPANY_OPTIONS = [
     "Select",
     "Kamal’s cake (SOUTH SANKRAIL)",
-    "Kamal’s Ice Cream (HOWRAH FOOD PARK, ”Adila”)",
+    "Adela Labs Pvt. Ltd (HOWRAH FOOD PARK)",
     "Kamals ORL O (Dhulagori)",
     "Kamals Ice Cream (Shaoraphuli, ”Adila”)",
     "Agarwal Food Product (Sankrail)",
